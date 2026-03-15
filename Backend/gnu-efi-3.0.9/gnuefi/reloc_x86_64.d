710e13e0de09f4b732d3622480f58e4cc1aac428
Jorge Gutierrez Vindas <tu-correo@estudiantec.cr>
2026-03-15 16:08:12 -0600

    A

---
+++ Backend/gnu-efi-3.0.9/gnuefi/reloc_x86_64.d
@@ -0,0 +1,68 @@
+../Backend/gnu-efi-3.0.9/gnuefi/reloc_x86_64.o: \
+ ../Backend/gnu-efi-3.0.9/gnuefi/reloc_x86_64.c \
+ ../Backend/gnu-efi-3.0.9/inc/efi.h \
+ ../Backend/gnu-efi-3.0.9/inc/x86_64/efibind.h \
+ ../Backend/gnu-efi-3.0.9/inc/eficompiler.h \
+ ../Backend/gnu-efi-3.0.9/inc/efidef.h \
+ ../Backend/gnu-efi-3.0.9/inc/efidevp.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipciio.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiprot.h \
+ ../Backend/gnu-efi-3.0.9/inc/ConsoleControl.h \
+ ../Backend/gnu-efi-3.0.9/inc/eficon.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiser.h \
+ ../Backend/gnu-efi-3.0.9/inc/efi_nii.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipxebc.h \
+ ../Backend/gnu-efi-3.0.9/inc/efinet.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiapi.h \
+ ../Backend/gnu-efi-3.0.9/inc/efifs.h \
+ ../Backend/gnu-efi-3.0.9/inc/efierr.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiui.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiip.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiudp.h \
+ ../Backend/gnu-efi-3.0.9/inc/efitcp.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipoint.h \
+ ../Backend/gnu-efi-3.0.9/inc/efisetjmp.h \
+ ../Backend/gnu-efi-3.0.9/inc/x86_64/efisetjmp_arch.h \
+ ../Backend/gnu-efi-3.0.9/inc/efilib.h \
+ ../Backend/gnu-efi-3.0.9/inc/efidebug.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipart.h \
+ ../Backend/gnu-efi-3.0.9/inc/x86_64/efilibplat.h \
+ ../Backend/gnu-efi-3.0.9/inc/efilink.h \
+ ../Backend/gnu-efi-3.0.9/inc/efirtlib.h \
+ ../Backend/gnu-efi-3.0.9/inc/efistdarg.h \
+ ../Backend/gnu-efi-3.0.9/inc/pci22.h \
+ ../Backend/gnu-efi-3.0.9/inc/libsmbios.h \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/elf.h
+../Backend/gnu-efi-3.0.9/inc/efi.h:
+../Backend/gnu-efi-3.0.9/inc/x86_64/efibind.h:
+../Backend/gnu-efi-3.0.9/inc/eficompiler.h:
+../Backend/gnu-efi-3.0.9/inc/efidef.h:
+../Backend/gnu-efi-3.0.9/inc/efidevp.h:
+../Backend/gnu-efi-3.0.9/inc/efipciio.h:
+../Backend/gnu-efi-3.0.9/inc/efiprot.h:
+../Backend/gnu-efi-3.0.9/inc/ConsoleControl.h:
+../Backend/gnu-efi-3.0.9/inc/eficon.h:
+../Backend/gnu-efi-3.0.9/inc/efiser.h:
+../Backend/gnu-efi-3.0.9/inc/efi_nii.h:
+../Backend/gnu-efi-3.0.9/inc/efipxebc.h:
+../Backend/gnu-efi-3.0.9/inc/efinet.h:
+../Backend/gnu-efi-3.0.9/inc/efiapi.h:
+../Backend/gnu-efi-3.0.9/inc/efifs.h:
+../Backend/gnu-efi-3.0.9/inc/efierr.h:
+../Backend/gnu-efi-3.0.9/inc/efiui.h:
+../Backend/gnu-efi-3.0.9/inc/efiip.h:
+../Backend/gnu-efi-3.0.9/inc/efiudp.h:
+../Backend/gnu-efi-3.0.9/inc/efitcp.h:
+../Backend/gnu-efi-3.0.9/inc/efipoint.h:
+../Backend/gnu-efi-3.0.9/inc/efisetjmp.h:
+../Backend/gnu-efi-3.0.9/inc/x86_64/efisetjmp_arch.h:
+../Backend/gnu-efi-3.0.9/inc/efilib.h:
+../Backend/gnu-efi-3.0.9/inc/efidebug.h:
+../Backend/gnu-efi-3.0.9/inc/efipart.h:
+../Backend/gnu-efi-3.0.9/inc/x86_64/efilibplat.h:
+../Backend/gnu-efi-3.0.9/inc/efilink.h:
+../Backend/gnu-efi-3.0.9/inc/efirtlib.h:
+../Backend/gnu-efi-3.0.9/inc/efistdarg.h:
+../Backend/gnu-efi-3.0.9/inc/pci22.h:
+../Backend/gnu-efi-3.0.9/inc/libsmbios.h:
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/elf.h:
---
+++ Simple_UEFI_Bootloader/src/Graphics.d
@@ -0,0 +1,81 @@
+src/src/Graphics.o: src/Graphics.c \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/Bootloader.h \
+ ../Backend/gnu-efi-3.0.9/inc/efi.h \
+ ../Backend/gnu-efi-3.0.9/inc/x86_64/efibind.h \
+ ../Backend/gnu-efi-3.0.9/inc/eficompiler.h \
+ ../Backend/gnu-efi-3.0.9/inc/efidef.h \
+ ../Backend/gnu-efi-3.0.9/inc/efidevp.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipciio.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiprot.h \
+ ../Backend/gnu-efi-3.0.9/inc/ConsoleControl.h \
+ ../Backend/gnu-efi-3.0.9/inc/eficon.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiser.h \
+ ../Backend/gnu-efi-3.0.9/inc/efi_nii.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipxebc.h \
+ ../Backend/gnu-efi-3.0.9/inc/efinet.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiapi.h \
+ ../Backend/gnu-efi-3.0.9/inc/efifs.h \
+ ../Backend/gnu-efi-3.0.9/inc/efierr.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiui.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiip.h \
+ ../Backend/gnu-efi-3.0.9/inc/efiudp.h \
+ ../Backend/gnu-efi-3.0.9/inc/efitcp.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipoint.h \
+ ../Backend/gnu-efi-3.0.9/inc/efisetjmp.h \
+ ../Backend/gnu-efi-3.0.9/inc/x86_64/efisetjmp_arch.h \
+ ../Backend/gnu-efi-3.0.9/inc/efilib.h \
+ ../Backend/gnu-efi-3.0.9/inc/efidebug.h \
+ ../Backend/gnu-efi-3.0.9/inc/efipart.h \
+ ../Backend/gnu-efi-3.0.9/inc/x86_64/efilibplat.h \
+ ../Backend/gnu-efi-3.0.9/inc/efilink.h \
+ ../Backend/gnu-efi-3.0.9/inc/efirtlib.h \
+ ../Backend/gnu-efi-3.0.9/inc/efistdarg.h \
+ ../Backend/gnu-efi-3.0.9/inc/pci22.h \
+ ../Backend/gnu-efi-3.0.9/inc/libsmbios.h \
+ ../Backend/gnu-efi-3.0.9/inc/x86_64/pe.h \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/elf.h \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/loader.h \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/mach/machine.h \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/mach/vm_prot.h \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/fat.h \
+ /home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/dos.h
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/Bootloader.h:
+../Backend/gnu-efi-3.0.9/inc/efi.h:
+../Backend/gnu-efi-3.0.9/inc/x86_64/efibind.h:
+../Backend/gnu-efi-3.0.9/inc/eficompiler.h:
+../Backend/gnu-efi-3.0.9/inc/efidef.h:
+../Backend/gnu-efi-3.0.9/inc/efidevp.h:
+../Backend/gnu-efi-3.0.9/inc/efipciio.h:
+../Backend/gnu-efi-3.0.9/inc/efiprot.h:
+../Backend/gnu-efi-3.0.9/inc/ConsoleControl.h:
+../Backend/gnu-efi-3.0.9/inc/eficon.h:
+../Backend/gnu-efi-3.0.9/inc/efiser.h:
+../Backend/gnu-efi-3.0.9/inc/efi_nii.h:
+../Backend/gnu-efi-3.0.9/inc/efipxebc.h:
+../Backend/gnu-efi-3.0.9/inc/efinet.h:
+../Backend/gnu-efi-3.0.9/inc/efiapi.h:
+../Backend/gnu-efi-3.0.9/inc/efifs.h:
+../Backend/gnu-efi-3.0.9/inc/efierr.h:
+../Backend/gnu-efi-3.0.9/inc/efiui.h:
+../Backend/gnu-efi-3.0.9/inc/efiip.h:
+../Backend/gnu-efi-3.0.9/inc/efiudp.h:
+../Backend/gnu-efi-3.0.9/inc/efitcp.h:
+../Backend/gnu-efi-3.0.9/inc/efipoint.h:
+../Backend/gnu-efi-3.0.9/inc/efisetjmp.h:
+../Backend/gnu-efi-3.0.9/inc/x86_64/efisetjmp_arch.h:
+../Backend/gnu-efi-3.0.9/inc/efilib.h:
+../Backend/gnu-efi-3.0.9/inc/efidebug.h:
+../Backend/gnu-efi-3.0.9/inc/efipart.h:
+../Backend/gnu-efi-3.0.9/inc/x86_64/efilibplat.h:
+../Backend/gnu-efi-3.0.9/inc/efilink.h:
+../Backend/gnu-efi-3.0.9/inc/efirtlib.h:
+../Backend/gnu-efi-3.0.9/inc/efistdarg.h:
+../Backend/gnu-efi-3.0.9/inc/pci22.h:
+../Backend/gnu-efi-3.0.9/inc/libsmbios.h:
+../Backend/gnu-efi-3.0.9/inc/x86_64/pe.h:
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/elf.h:
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/loader.h:
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/mach/machine.h:
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/mach/vm_prot.h:
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/fat.h:
+/home/kokipc/Bootloader-Operativos/Simple_UEFI_Bootloader/inc/dos.h:
